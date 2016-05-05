c9189f246cded50bfec1bfeab8fcb437b947de0b
Younji <younjiwoo@gmail.com>
2016-05-05 16:18:11 -0700

    updated form-parser.php
    I got rid of "requested" properties from the html file to see if "if
    statements" work in the php file. When you don't fill in the required
    areas like Name and Email Address, and try to submit, on the next page
    you'll see  a message saying "You need to fill in all required fields."
    Same thing happens when you try to submit without filling in the correct
    image verification.

--- responsive_web_design_video_and_form/pages/form-parser.php
+++ responsive_web_design_video_and_form/pages/form-parser.php
@@ -3,31 +3,27 @@
     $name = $_POST['name'];
     $phone = $_POST['phone'];
     $email = $_POST['email'];
-    $url = $_POST['url'];
-    $sponsorAmt = $_POST['sponsorAmt'];
-    $area = $POST_['area'];
-    $newsletter = $POST_['newsletter'];
     $comments = $_POST['comments'];
     $from = "From: form inquiry";
     $to = "younjiwoo@gmail.com";
-    $subject = "I have a question.";
+    $subject = "I have a question";
     $human = $_POST['human'];
 
-    $body = "From: $name\n Phone: $phone\n Email: $email\n Message: $comments";
-    
+    $body = "From: $name\n Email: $email\n Phone: $phone\n Message: $comments";
+        
     if ($_POST['submit']) {
         if ($name !='' && $email !='') {
             if ($human == 'pastry') {
                 if (mail ($to, $subject, $body, $from)) {
-                    echo '<p>Your message has been sent.</p>';
+                    echo 'Your message has been sent.';
             }    else {
-                    echo '<p>Something is wrong, go back and try again.</p>';
+                    echo 'Something is wrong, go back and try again.';
                  }
-            } else if ($_POST['submit'] && $human !='pastry') {
-                    echo '<p>The characters are wrong, please try again.</p>';
+            } else if ($_POST['submit'] && $human != 'pastry') {    
+                echo 'Please try the image verification again.';
             }    
             } else {
-                    echo '<p>You need to fill in all required fields </p>';
+                    echo 'You need to fill in all required fields.';
             }
     }
 
