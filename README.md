70886241073eb978fa4113a33ccad4265cbf7a51
younjiwoo <younjiwoo@gmail.com>
2017-05-14 19:44:51 -0700

    uploaded my portfolio page

---
+++ README.md
@@ -0,0 +1,20 @@
+<snippet>
+  <content><![CDATA[
+# ${1:Younji Woo Portfolio}
+
+This is my portfolio page. Here I link my projects and also offer methods of contact and a short bio.
+
+## Usage
+
+Visit the address: https://younjiwoo.github.io/
+
+## Credits
+
+Homepage template found here: https://html5up.net/big-picture
+
+## License
+
+My portfolio webpage is released under the MIT License.
+]]></content>
+  <tabTrigger>readme</tabTrigger>
+</snippet>
---
+++ assets/js/app.js
@@ -0,0 +1,84 @@
+let projects = [
+    {
+        projectName: "Swimming Turtle",
+        url: "/projects/swimming_turtle/usingCSS.html",
+        image: "images/swimmingTurtle.png",
+        description: "A swimming turtle made with pure CSS."
+    },
+    {
+        projectName: "Music Boxes",
+        url: "/projects/music_boxes/index.html",
+        image: "images/musicBoxes.png",
+        description: "Google coder music box project using jQuery, HTML5 Audio tags and CSS."
+    },
+    {
+        projectName: "Horn Shark Page",
+        url: "/projects/horn_sharks_responsive/index.html",
+        image: "images/hornShark.png",
+        description: "A responsive webpage about Horn Sharks."
+    },
+    {
+        projectName: "Day and Night",
+        url: "/projects/day_and_night/index.html",
+        image: "images/dayAndNight.png",
+        description: "Change day into night and back again with the click of a mouse. Made with jQuery."
+    },
+    {
+        projectName: "Digital Clock",
+        url: "/projects/digital_clock/index.html",
+        image: "images/digitalClock.png",
+        description: "A simple digital clock using basic Javascript."
+    },
+    {
+        projectName: "HOA Website",
+        url: "https://fewd1-team10.github.io/",
+        image: "images/hoaWebsite.png",
+        description: "A re-design of my previous HOA's website."
+    }
+];
+        
+        
+    
+let subtitlesArr = document.querySelectorAll('h2.subtitle');
+for (let i = 0; i < subtitlesArr.length; i++) {
+    if (i !== subtitlesArr.length - 1) {
+        subtitlesArr[i].classList.add('margin-right');
+    }
+}
+function addProjects() {
+    let $gallery = $('.gallery');
+    projects.forEach(function(project, idx) {
+        let $article = $('<article>');
+        if (idx % 2 === 0) {
+            $article.addClass('from-left');
+        } else {
+            $article.addClass('from-right');
+        }
+        let $a = $('<a>');
+        $a.attr({
+            class: "image fit",
+            href: project.url,
+            target: "_blank",
+            style: "position: relative"
+        });
+        let $img = $('<img>');
+        $img.attr({
+            src: project.image,
+            alt: project.projectName,
+            title: project.projectName
+        });
+        let $div = $('<div>').addClass('projectDescription');
+        let $h3 = $('<h3>');
+        $h3.text(project.projectName);
+        let $p = $('<p>').text(project.description);
+        $div.append($h3).append($p);
+        $a.append($img).append($div);
+        $article.append($a);
+        $gallery.append($article);
+    }); // forEach ends.
+    
+}
+
+window.onload = function(){
+    addProjects();
+}
